fc7f54c6321fa109e74b88e3d08594a01101c3cf
Weslo <wesley.rockholz@gmail.com>
2015-05-21 17:12:14 -0400

    Initial Commit
    Initial commit of the cross-platform, non-profit application for
    helping Peter by improving his live drastically.

---
+++ Peter/Models/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Peter
+{
+	public abstract class IViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string name) {
+			if (PropertyChanged == null)
+				return;
+
+			PropertyChanged (this, new PropertyChangedEventArgs (name));
+		}
+	}
+}
---
+++ Peter/Pages/InstructionsPage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace Peter
+{
+	public class InstructionsPage : ContentPage
+	{
+		private readonly ObservableCollection<InstructionsItem> instructions;
+
+		public InstructionsPage (int hours)
+		{
+			Title = "Peter's To Do";
+
+			instructions = new ObservableCollection<InstructionsItem> ();
+			ListView instructionsView = new ListView {
+				ItemsSource = instructions,
+			};
+			instructionsView.ItemTemplate = new DataTemplate (typeof(InstructionsCell));
+
+			// Accomodate iPhone status bar
+			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+
+			Content = new StackLayout {
+				Children = {
+					instructionsView,
+				}
+			};
+
+			for (int i = 0; i < hours; i++) {
+				instructions.Add (new InstructionsItem (PickRandomInstruction()));
+			}
+		}
+
+		class InstructionsItem {
+			public InstructionsItem (string instructions) {
+				this.Instructions = instructions;
+			}
+
+			public string Instructions { get; set; }
+		}
+
+		class InstructionsCell : ViewCell {
+			public InstructionsCell () {
+				Label text = new Label {
+					HorizontalOptions = LayoutOptions.StartAndExpand
+				};
+				text.SetBinding (Label.TextProperty, new Binding ("Instructions"));
+
+				View = new StackLayout {
+					Children = {
+						text
+					},
+					Padding = 10
+				};
+			}
+		}
+
+		private readonly string[] PossibleInstructions = new string [] {
+			"Take a shot",
+			"Chug a beer",
+			"Do a power hour",
+			"Shotgun a beer",
+		};
+
+		private readonly Random r = new Random ();
+
+		private string PickRandomInstruction() {
+			int index = r.Next (0, PossibleInstructions.Length);
+			return PossibleInstructions [index];
+		}
+	}
+}
+
