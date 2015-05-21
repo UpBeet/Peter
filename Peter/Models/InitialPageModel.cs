4bb96192de7ed99d83890aebfe248f603a209f4f
Weslo <wesley.rockholz@gmail.com>
2015-05-21 17:12:41 -0400

    Committing initial page files
    Committing InitialPage.cs and InitialPageModel.cs.

---
+++ Peter/Models/InitialPageModel.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+namespace Peter
+{
+	public class InitialPageModel : IViewModel
+	{
+		private INavigation navigation;
+
+		public InitialPageModel (INavigation navigation)
+		{
+			this.navigation = navigation;
+		}
+
+		public int DrinkingHours {
+			get { return this.drinkingHours; }
+			set {
+				if (this.drinkingHours != value) {
+					this.drinkingHours = value;
+					OnPropertyChanged ("DrinkingHours");
+					OnPropertyChanged ("DrinkingHoursDisplay");
+				}
+			}
+		}
+		private int drinkingHours = 5;
+
+		public string DrinkingHoursDisplay {
+			get {
+				return this.DrinkingHours + " hours";
+			}
+		}
+
+		private Command pressMeCommand;
+		public Command PressMeCommand {
+			get { return pressMeCommand ?? (pressMeCommand = new Command (ExecutePressMeCommand)); }
+		}
+
+		private void ExecutePressMeCommand() {
+			navigation.PushAsync (new InstructionsPage(this.DrinkingHours));
+		}
+	}
+}
+
---
+++ Peter/Pages/InitialPage.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace Peter
+{
+	public class InitialPage : ContentPage
+	{
+		public InitialPage ()
+		{
+			BindingContext = new InitialPageModel (this.Navigation);
+
+			Title = "Welcome to Peter";
+
+			Label timeLabel = new Label {
+				HorizontalOptions = LayoutOptions.Center,
+			};
+			timeLabel.SetBinding<InitialPageModel> (Label.TextProperty, vm => vm.DrinkingHoursDisplay);
+
+			Slider timeSlider = new Slider (1, 24, 5) {
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+			};
+			timeSlider.SetBinding<InitialPageModel> (Slider.ValueProperty, vm => vm.DrinkingHours, BindingMode.TwoWay);
+
+			Button pressMe = new Button {
+				Text = "Help Peter",
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				FontSize = 60,
+				HeightRequest = 200,
+				BackgroundColor = Color.Yellow,
+			};
+			pressMe.SetBinding<InitialPageModel> (Button.CommandProperty, vm => vm.PressMeCommand);
+
+			Content = new StackLayout {
+				Children = {
+					timeLabel,
+					timeSlider,
+					pressMe,
+				}
+			};
+		}
+	}
+}
+
